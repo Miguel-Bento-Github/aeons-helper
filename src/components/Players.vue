5a492c101452e0f21b286dd75ca9b37243c25f34
Ruxian Yang <yruxian@gmail.com>
2021-06-17 21:34:45 +0200

    Add player section styles

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -13,9 +13,11 @@ export default {
 <template>
   <section>
     <div v-for="{ name, life, id } in players" :key="name">
-      <div>
-        Name: {{ name }}
+      <div class="player-wrapper">
+        <div class="name">Name: {{ name }}</div>
+        <div class="life">Life: {{ life }}</div>
         <router-link
+          class="Edit-link"
           :to="{
             name: 'Edit',
             params: { name: name, id: id },
@@ -25,7 +27,34 @@ export default {
         >
         <button type="button" @click="deletePlayer(id)">Delete</button>
       </div>
-      <div>Life: {{ life }}</div>
+      <!--<div>Life: {{ life }}</div> -->
     </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.player-wrapper {
+  display: flex;
+  justify-content: space-evenly;
+  padding: 0 3rem;
+}
+
+.Edit-link {
+  margin-left: 1.2rem;
+  padding-right: 0.3rem;
+  text-decoration: none;
+
+  & :link {
+    color: #fff;
+  }
+
+  &:visited {
+    color: #fff;
+  }
+}
+
+.name {
+  width: 8rem;
+  text-align: left;
+}
+</style>
